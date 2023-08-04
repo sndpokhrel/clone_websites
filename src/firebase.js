87803c0cce26316798c5850f60feb8b98cf66fba
SP_doesnot_works <sandypokhrel09@gmail.com>
2023-08-04 07:07:02 +0545

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDNaGlQy02s6EUfBsAwCz4I0kBy2tw-TOw",
+  authDomain: "netflix-clone-42a69.firebaseapp.com",
+  projectId: "netflix-clone-42a69",
+  storageBucket: "netflix-clone-42a69.appspot.com",
+  messagingSenderId: "169150359016",
+  appId: "1:169150359016:web:ac0cc8ad6a4a83bc36305d",
+};
