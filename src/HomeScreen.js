7e310319287616f614deaf73b4e6832ff94fa276
sndpokhrel <sandypokhrel09@gmail.com>
2023-09-07 15:45:59 +0545

    Home Screen main page

---
+++ src/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Banner from "./Banner";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      <Banner />
+
+      {/* Row */}
+    </div>
+  );
+};
+
+export default HomeScreen;
