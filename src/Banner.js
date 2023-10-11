c4a04ce7be2b5ecd4a99a9d8ce079763b417fca9
sndpokhrel <sandypokhrel09@gmail.com>
2023-10-11 09:00:29 +0545

    banner js code

--- src/Banner.js
+++ src/Banner.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url{"https://i.imgur.com/e1hLQ2m.png"}`,
+        backgroundImage: `url(${"https://wallpapers.com/images/high/vantablack-background-1920-x-1080-2jce1nl3275dp7p2.webp"})`,
         backgroundPosition: "center center",
       }}
     >
@@ -17,7 +20,17 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">This is test descrip</h1>
+        <h1 className="banner_description">
+          {truncate(
+            `This is test descriptionThis is test descriptionThis is test
+          descriptionThis is test descriptionThis is test descriptionThis is
+          test descriptionThis is test descriptionThis is test descriptionThis
+          is test descriptionThis is test descriptionThis is test
+          descriptionThis is test descriptionThis is test descriptionThis is
+          test descriptionThis is test descriptionThis is test description`,
+            150
+          )}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom" />
